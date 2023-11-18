bfb84723d8697767baa1429955bd881a9566ed3b
Samuel Cain <s2097950@ed.ac.uk>
2023-11-18 17:50:36 +0000

    Updated tests

--- tests/test_ringNetwork.py
+++ tests/test_ringNetwork.py
@@ -24,3 +24,15 @@ def test_W0():
         W0 = np.random.random(1)
         rn = RingNetwork(100, W0, 0)
         assert np.all(rn.W == W0)
+
+def test_sim():
+    def t(N, dt, T):
+        rn = RingNetwork(N, 0, 0)
+        V = rn.simulate(dt=dt, T=T)
+        assert V.shape == (int(T/dt + 0.5), N)
+
+    for _ in range(100):
+        N = np.random.randint(10, 100)
+        dt = np.random.random() 
+        T = 1 + np.random.random() * 10
+        t(N, dt, T)
