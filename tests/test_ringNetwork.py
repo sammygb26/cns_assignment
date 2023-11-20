f03410e5372918964bd4e2d0b578ff28a19bff15
Samuel Cain <s2097950@ed.ac.uk>
2023-11-20 11:58:25 +0000

    Added test for W

--- tests/test_ringNetwork.py
+++ tests/test_ringNetwork.py
@@ -25,6 +25,11 @@ def test_W0():
         rn = RingNetwork(100, W0, 0)
         assert np.all(rn.W == W0)
 
+        for _ in range(100):
+            rand_n = np.random.randint(0, rn.N)
+            assert np.abs(rn.W[rand_n,-1] - rn.W[rand_n,0]) < 0.01
+            assert np.abs(rn.W[-1, rand_n] - rn.W[0, rand_n]) < 0.01
+
 def test_sim():
     def t(N, dt, T):
         rn = RingNetwork(N, 0, 0)
